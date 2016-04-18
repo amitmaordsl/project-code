5c7418609cf78ba0beac6ed44c78192ef2c1be5a
amitmaor <amitmaor@WL-200-87.WPA.Claremont.Edu>
2016-04-17 23:04:00 -0700

    done with the week's code

--- src/interpreter.py
+++ src/interpreter.py
@@ -9,14 +9,16 @@ from poemHandler import lastWords, filenameToLines
 ### MAIN functionality
 ###############
 
-def checkPoem(filename, scheme):
-	# take the filename and open it into lines, then last words of lines
-	lines = filenameToLines(filename)
-	lastWordsList = lastWords(lines)
+schemeDict = collections.OrderedDict()
 
-	# use the last words to create a scheme
-	poemScheme = createScheme(lastWordsList)
-	return schemeMatcher(scheme, poemScheme)
+def checkPoem(filename, scheme):
+    # take the filename and open it into lines, then last words of lines
+    lines = filenameToLines(filename)
+    lastWordsList = lastWords(lines)
+
+    # use the last words to create a scheme
+    poemScheme = createScheme(lastWordsList)
+    return schemeParser(scheme, poemScheme, lastWordsList)
 
 ###############
 ### POEM functionality
@@ -24,44 +26,70 @@ def checkPoem(filename, scheme):
 ### takes inputs from poem side and interprets
 ###############
 
+def rhymeListFromWord(word):
+    wordStr = rhymesFromWord(word)
+    wordJSONThing = json.loads(wordStr)
+    return map(lambda k: k["word"], wordJSONThing)
+
 def twoWordsRhyme(word1, word2):
-    firstWordStr = rhymesFromWord(word1)
-    firstWordJSONThing = json.loads(firstWordStr)
-    firstWordWords = map(lambda k: k["word"], firstWordJSONThing)
+    firstWordWords = rhymeListFromWord(word1)
     
-    secondWordStr = rhymesFromWord(word2)
-    secondWordJSONThing = json.loads(secondWordStr)
-    secondWordWords = map(lambda k: k["word"], secondWordJSONThing)
+    secondWordWords = rhymeListFromWord(word2)
     return (word2 in firstWordWords) or (word1 in secondWordWords)
 
 def checkRhymes():
-	lastWordsList = lastWords(openAndSplit())
+    lastWordsList = lastWords(openAndSplit())
 
-	checkWord = lastWordsList[0]
-	lastWordsList = lastWordsList[1:]
+    checkWord = lastWordsList[0]
+    lastWordsList = lastWordsList[1:]
 
-	for word in lastWordsList:
-		if not twoWordsRhyme(word, checkWord):
-			print word
-			print checkWord
-			return False
+    for word in lastWordsList:
+        if not twoWordsRhyme(word, checkWord):
+            print word
+            print checkWord
+            return False
 
-	return True
+    return True
 
 ###############
 ### SCHEME functionality
 ###
 ### takes inputs from scheme side and interprets
 ###############
-def schemeMatcher(pattern, parsedPattern):
-	# matchedPattern = re.match(r'(ab)*', "abababc", flags=0).group()
-	matchedPattern = re.match(pattern, parsedPattern, flags=0).group()
-	return (matchedPattern == parsedPattern)
+def schemeParser(pattern, parsedPattern, lastWordsList):
+    parsedPatternIndex = 0
+    while (not (not pattern)):
+        # run into paren
+        if (pattern[0] == '('):
+            endBrace = pattern.find(')')
+            tempPattern = pattern[1:endBrace]
+            
+            # the parsed pattern matches the parens pattern
+            while (tempPattern == parsedPattern[:len(tempPattern)]):
+                parsedPattern = parsedPattern[len(tempPattern):]
+                parsedPatternIndex = parsedPatternIndex + len(tempPattern)
+
+            pattern = pattern[endBrace+1:]
+        # here, you match a single character in the pattern to a single char in the parsed pattern 
+        # then you recurse
+        else:
+            if (pattern[0] == parsedPattern[0]):
+                pattern = pattern[1:]
+                parsedPattern = parsedPattern[1:]
+                parsedPatternIndex = parsedPatternIndex + 1
+            else:
+                print "Poem did not match specified scheme!"
+                print "incorrect line is line " + str(parsedPatternIndex) + ", with last word: " + lastWordsList[parsedPatternIndex]
+                print "the correct word should rhyme with " + schemeDict[pattern[0]]
+                print "rhyme suggestions: " + ', '.join(rhymeListFromWord(schemeDict[pattern[0]]))
+                print "Current letter assignments:", json.dumps(schemeDict, indent=4)
+                return
+    return (not pattern) and (not parsedPattern)
+
 
 def createScheme(words):
     ret = ""
     schemeChar = 'a'
-    schemeDict = collections.OrderedDict()
 
     for word in words:
         # did the word match a rhyme higher in the poem
@@ -83,4 +111,4 @@ def createScheme(words):
         # print ret
     return ret
 
-print(checkPoem("tmp.txt", r'(a*)ba'))
+print(checkPoem("tmp.txt", r'(a)bba'))
